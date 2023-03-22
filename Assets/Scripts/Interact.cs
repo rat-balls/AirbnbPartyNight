354338ac3c99bb7b14c422562e7611fb7a9ad495
E.Delalande <116553314+rat-balls@users.noreply.github.com>
2023-03-22 07:05:43 +0100

    doors are better

--- Assets/Scripts/Interact.cs
+++ Assets/Scripts/Interact.cs
@@ -6,7 +6,7 @@ using DG.Tweening;
 public class Interact : MonoBehaviour
 {   
     public Transform interactCheck;
-    public float interactDistance = 0.4f;
+    public float interactDistance = 7.14f;
     public LayerMask interactableMask;
     public AudioSource Source;
     public AudioClip doorCloseSound;
@@ -15,7 +15,9 @@ public class Interact : MonoBehaviour
     
     public bool inRange;
 
-
+    private void Start() {
+        DOTween.Init();
+    }
     // Update is called once per frame
     void Update()
     {
@@ -26,22 +28,93 @@ public class Interact : MonoBehaviour
         Collider[] objects = Physics.OverlapSphere(interactCheck.position, interactDistance, interactableMask);
         foreach (var obj in objects)
         {   
-            if(obj.tag == "door")
+            if(obj.tag == "door -z facing")
             {   
                 
                 Animator animator = obj.GetComponent<Animator>();
                 
                 if(Input.GetKeyDown(KeyCode.E))
                 {   
+                    Vector3 Openangle = new Vector3(-90f, 0, -90f);
+                    Vector3 Closeangle = new Vector3(-90f, 0, 0);
                     AnimatorStateInfo currentAnim = animator.GetCurrentAnimatorStateInfo(0);
                     if(currentAnim.IsName("DoorClose") || currentAnim.IsName("Idle")){
-                        Debug.Log("Open");
+                        obj.transform.DORotate(Openangle, 1f);
+                        animator.Play("DoorOpen");
                         // Source.PlayOneShot(doorOpenSound);
                     } else if(currentAnim.IsName("DoorOpen")){
                         Debug.Log("Close");
+                        obj.transform.DORotate(Closeangle, 1f);
+                        animator.Play("DoorClose");
                         // Source.PlayOneShot(doorCloseSound);
                     } 
+                }
+            } else if(obj.tag == "door -x facing")
+            {   
+                
+                Animator animator = obj.GetComponent<Animator>();
+
+                
+                if(Input.GetKeyDown(KeyCode.E))
+                {   
                     
+                    Vector3 Openangle = new Vector3(-90f, 0, 0f);
+                    Vector3 Closeangle = new Vector3(-90f, 0, 90f);
+                    AnimatorStateInfo currentAnim = animator.GetCurrentAnimatorStateInfo(0);
+
+                    if(currentAnim.IsName("DoorClose") || currentAnim.IsName("Idle")){
+                        obj.transform.DORotate(Openangle, 1f);
+                        animator.Play("DoorOpen");
+                        // Source.PlayOneShot(doorOpenSound);
+                    } else if(currentAnim.IsName("DoorOpen")){
+                        Debug.Log("Close");
+                        obj.transform.DORotate(Closeangle, 1f);
+                        animator.Play("DoorClose");
+                        // Source.PlayOneShot(doorCloseSound);
+                    }
+                }
+            } else if(obj.tag == "door x facing")
+            {   
+                
+                Animator animator = obj.GetComponent<Animator>();
+                
+                if(Input.GetKeyDown(KeyCode.E))
+                {   
+                    Vector3 Openangle = new Vector3(-90f, 0, -270f);
+                    Vector3 Closeangle = new Vector3(-90f, 0, -180f);
+                    AnimatorStateInfo currentAnim = animator.GetCurrentAnimatorStateInfo(0);
+
+                    if(currentAnim.IsName("DoorClose") || currentAnim.IsName("Idle")){
+                        obj.transform.DORotate(Openangle, 1f);
+                        animator.Play("DoorOpen");
+                        // Source.PlayOneShot(doorOpenSound);
+                    } else if(currentAnim.IsName("DoorOpen")){
+                        Debug.Log("Close");
+                        obj.transform.DORotate(Closeangle, 1f);
+                        animator.Play("DoorClose");
+                        // Source.PlayOneShot(doorCloseSound);
+                    } 
+                }
+            } else if(obj.tag == "door z facing")
+            {   
+                
+                Animator animator = obj.GetComponent<Animator>();
+                
+                if(Input.GetKeyDown(KeyCode.E))
+                {   
+                    Vector3 Openangle = new Vector3(-90f, 0, 0f);
+                    Vector3 Closeangle = new Vector3(-90f, 0, -90f);
+                    AnimatorStateInfo currentAnim = animator.GetCurrentAnimatorStateInfo(0);
+                    if(currentAnim.IsName("DoorClose") || currentAnim.IsName("Idle")){
+                        obj.transform.DORotate(Openangle, 1f);
+                        animator.Play("DoorOpen");
+                        // Source.PlayOneShot(doorOpenSound);
+                    } else if(currentAnim.IsName("DoorOpen")){
+                        Debug.Log("Close");
+                        obj.transform.DORotate(Closeangle, 1f);
+                        animator.Play("DoorClose");
+                        // Source.PlayOneShot(doorCloseSound);
+                    } 
                 }
             }
             
